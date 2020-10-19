6e2b585bae429004e2c5aa6c640e551548d12173
jjrohrer <jj.bitbucket@rohrer.org>
2020-10-19 15:40:05 -0400

    prettier

--- resources/views/livewire/lowernav.blade.php
+++ resources/views/livewire/lowernav.blade.php
@@ -1,7 +1,7 @@
 @php
 
     // JJ - How can you make these common without perculating up the heirarchy?  Simple includes hide varaibles.
-             $liWrapper_1 = $liWrapper_2 = 'pr-1 ';
+             $liWrapper_1 = $liWrapper_2 = 'pr-1 pb-1';
              $liWrapper_3 = ' pr-1 -ml-2 pl-2  ';  // feels like should be below, not in li
 
              $linkFontColor_cssClasses = 'text-gray-300';
@@ -10,10 +10,10 @@
              $placeholderFontColor_cssClasses = 'text-gray-400';
 
 
-             $divNode_cssClasses     = "$linkFontColor_cssClasses        $linkFocus_cssClasses no-underline group flex items-center px-2 py-0 text-md leading-5 font-bold   ";
+             $divNode_cssClasses     = "$linkFontColor_cssClasses        $linkFocus_cssClasses no-underline group flex items-center px-2  text-md leading-5 font-bold   ";
 
              $firstLeaf_cssClasses   = "$linkFontColor_cssClasses        $linkFocus_cssClasses px-2 text-lg leading-5 font-bold    ";
-             $secondLeaf_cssClasses  = "$linkFontColor_cssClasses        $linkFocus_cssClasses group flex items-center px-2 pb-2 text-sm leading-5 rounded-md  ";
+             $secondLeaf_cssClasses  = "$linkFontColor_cssClasses        $linkFocus_cssClasses group flex items-center px-2  text-sm  rounded-md  ";
              $secondBreak_cssClasses = "$placeholderFontColor_cssClasses ml-1.5 text-base font-bold ";
              $thirdLeaf_cssClasses   = "$linkFontColor_cssClasses        $linkFocus_cssClasses ml-5 ";
              $icon_cssClasses = "topLevelNavIcon w-6 h-6 mr-3 $placeholderFontColor_cssClasses";
