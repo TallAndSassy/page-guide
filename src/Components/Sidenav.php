6f196f2fecd439455e4c4d5cfc897ff16b320777
jjrohrer <jj.bitbucket@rohrer.org>
2020-10-23 09:13:35 -0400

    more

--- src/Components/Sidenav.php
+++ src/Components/Sidenav.php
@@ -14,17 +14,7 @@ class Sidenav extends Component
 {
     public string $sample_var = "defaultValue"; // {{$sample_var}} is now avail in your blade.
 
-    public static function wireSwaplinkInA(string $url)
-    {
-        // Note: close(); is there to force closing of any open modal
-        // 8/20' Known Issue: Calls close() twice.  Everything is always getting closed twice.
 
-        return <<<EOD
-        wire:click="\$emit('pageRoute','$url')"
-        x-on:click.prevent="maybeCloseAdminMenu(); urlChange('$url');"
-        href="{$url}"
-        EOD;
-    }
 
     public function render()
     {
