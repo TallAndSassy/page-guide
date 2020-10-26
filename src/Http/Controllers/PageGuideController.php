0ca33ee38f5a5b5afbdee50699c46d23f24513f5
jjrohrer <jj.bitbucket@rohrer.org>
2020-10-26 08:19:11 -0400

    where are we

--- src/Http/Controllers/PageGuideController.php
+++ src/Http/Controllers/PageGuideController.php
@@ -5,15 +5,43 @@ namespace TallAndSassy\PageGuide\Http\Controllers;
 
 class PageGuideController
 {
+    private static bool $isInited = false;
+    private static bool $isBackPage;
+    private static bool $isAdminPage;
+    private static bool $isMePage;
+
+    private static function initCache(): void
+    {
+        $firstSubDir = explode('/', request()->getPathInfo())[1];
+        static::$isBackPage = in_array($firstSubDir, ['user', 'teams', 'me', 'admin']);
+        static::$isAdminPage = $firstSubDir == 'admin';
+        static::$isMePage = in_array($firstSubDir, ['user', 'teams', 'me']);
+        static::$isInited = true;
+        #dd([__FILE__,__LINE__,$firstSubDir, get_class_vars(PageGuideController::class)]);
+    }
+
     public static function isBackPage(): bool
     {
-        #dd(explode('/', request()->getPathInfo())[1]);
-        $isBack = in_array(explode('/', request()->getPathInfo())[1], ['user','teams','me','admin','user']);
-        #dd($isBack);
-        return $isBack;
+        if (! static::$isInited) {
+            static::initCache();
+        }
+
+        return static::$isBackPage;
     }
-    public function sample()
+    public static function isAdminPage(): bool
     {
-        return 'Hello PageGuide from: '.__METHOD__;
+        if (! static::$isInited) {
+            static::initCache();
+        }
+
+        return static::$isAdminPage;
+    }
+    public static function isMePage(): bool
+    {
+        if (! static::$isInited) {
+            static::initCache();
+        }
+
+        return static::$isMePage;
     }
 }
